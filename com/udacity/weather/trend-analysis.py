c1c650601de5fa7f7191206c3e4826c1b7795a00
Gergely Kajtar <gergo.kajtar@gmail.com>
2020-02-22 23:23:41 +0100

    Create dataframe with world and Barcelona data

--- com/udacity/weather/trend-analysis.py
+++ com/udacity/weather/trend-analysis.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+world_df = pd.read_csv('../../../data/world.csv')
+world_series = pd.Series(data=world_df['avg_temp'].values, index=world_df['year'].values)
+
+barcelona_df = pd.read_csv('../../../data/barcelona.csv')
+barcelona_series = pd.Series(data=barcelona_df['avg_temp'].values, index=barcelona_df['year'].values)
+
+frame = {"world": world_series, "Barcelona": barcelona_series}
+df = pd.DataFrame(frame)
