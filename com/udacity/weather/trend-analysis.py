f6744ba4fc21c3505061916212ee235f4d56aa2e
Gergely Kajtar <gergo.kajtar@gmail.com>
2020-02-24 21:31:43 +0100

    Add rolling mean to dataframe

--- com/udacity/weather/trend-analysis.py
+++ com/udacity/weather/trend-analysis.py
@@ -2,9 +2,16 @@ import pandas as pd
 
 world_df = pd.read_csv('../../../data/world.csv')
 world_series = pd.Series(data=world_df['avg_temp'].values, index=world_df['year'].values)
+world_rolling_mean_series: pd.Series = world_series.rolling(10).mean().iloc[9:]
 
 barcelona_df = pd.read_csv('../../../data/barcelona.csv')
-barcelona_series = pd.Series(data=barcelona_df['avg_temp'].values, index=barcelona_df['year'].values)
+barcelona_series: pd.Series = pd.Series(data=barcelona_df['avg_temp'].values, index=barcelona_df['year'].values)
+barcelona_rolling_mean_series: pd.Series = barcelona_series.rolling(10).mean().iloc[9:]
 
-frame = {"world": world_series, "Barcelona": barcelona_series}
+frame = {"World": world_series,
+         "World Rolling Mean (10)": world_rolling_mean_series,
+         "Barcelona": barcelona_series,
+         "Barcelona Rolling Mean (10)": barcelona_rolling_mean_series}
 df = pd.DataFrame(frame)
+
+print(df)
